68c389167aaf8d23ef74cb3e730df0ea2fa64d23
ritavash01 <94717544+ritavash01@users.noreply.github.com>
2024-12-27 02:55:09 +0530

    Update data_sequence.py
    Instead of reading a h5 file, this version of FETCH reads from a tuple in memory

--- fetch/data_sequence.py
+++ fetch/data_sequence.py
@@ -1,10 +1,6 @@
-"""
-https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
-"""
 import logging
 import os
 
-import h5py
 from tensorflow import keras
 import numpy as np
 import scipy.signal as s
@@ -12,11 +8,10 @@ import scipy.signal as s
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 logger = logging.getLogger(__name__)
 
-
 class DataGenerator(keras.utils.Sequence):
     def __init__(
         self,
-        list_IDs,
+        result_list,
         labels,
         batch_size=32,
         ft_dim=(256, 256),
@@ -29,21 +24,20 @@ class DataGenerator(keras.utils.Sequence):
         noise_std=1.0,
     ):
         """
-
-        :param list_IDs: list of h5 files
-        :type list_IDs: list
+        :param result_list: list of tuples containing data (data_freq_time, data_dm_time)
+        :type result_list: list
         :param labels: list of labels (use fake labels when using predict)
         :type labels: list
         :param batch_size: Batch size (def = 32)
         :type batch_size: int
-        :param ft_dim: 2D shape (def (256, 256)
-        :type dt_dim tuple
-        :param dt_dim: 2D shape (def (256, 256)
-        :type ft_dim tuple
+        :param ft_dim: 2D shape (def (256, 256))
+        :type ft_dim: tuple
+        :param dt_dim: 2D shape (def (256, 256))
+        :type dt_dim: tuple
         :param n_channels: number of channels in data (def = 1)
         :type n_channels: int
         :param n_classes: number of classes to classify data into (def = 2)
-        :type n_classes: ints
+        :type n_classes: int
         :param shuffle: to shuffle or not to shuffle?
         :type shuffle: bool
         :param noise: to add noise or not to?
@@ -56,7 +50,7 @@ class DataGenerator(keras.utils.Sequence):
         self.ft_dim = ft_dim
         self.dt_dim = dt_dim
         self.batch_size = batch_size
-        self.list_IDs = list_IDs
+        self.result_list = result_list
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
@@ -68,14 +62,12 @@ class DataGenerator(keras.utils.Sequence):
 
     def __len__(self):
         """
-
         :return: Number of batches per epoch
         """
-        return int(np.ceil(len(self.list_IDs) / self.batch_size))
+        return int(np.ceil(len(self.result_list) / self.batch_size))
 
     def __getitem__(self, index):
         """
-
         :param index: index
         :return: Data dictionary and categorical labels
         """
@@ -85,8 +77,8 @@ class DataGenerator(keras.utils.Sequence):
             ]
         else:
             indexes = self.indexes[index * self.batch_size :]
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        # result_list is the tuple containing the dmtransform and dedispered data, present in the memory
+        list_IDs_temp = [self.result_list[k] for k in indexes]
 
         # Generate data
         X, y = self.__data_generation(list_IDs_temp, indexes)
@@ -95,17 +87,15 @@ class DataGenerator(keras.utils.Sequence):
 
     def on_epoch_end(self):
         """
-
         :return: Updates the indices at the end of the epoch
         """
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
+        self.indexes = np.arange(len(self.result_list))
+        if self.shuffle:
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, list_IDs_temp, indexes):
         """
-
-        :param list_IDs_temp: list of h5 files to read
+        :param list_IDs_temp: list of tuples containing data (data_freq_time, data_dm_time)
         :param indexes: indexes
         :return: Batch of data and labels
         """
@@ -114,28 +104,20 @@ class DataGenerator(keras.utils.Sequence):
         y = np.empty((len(list_IDs_temp)), dtype=int)
 
         # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            try:
-                with h5py.File(ID, "r") as f:
-                    data_ft = s.detrend(
-                        np.nan_to_num(np.array(f["data_freq_time"], dtype=np.float32).T)
-                    )
-                    data_ft /= np.std(data_ft)
-                    data_ft -= np.median(data_ft)
-                    data_dt = np.nan_to_num(
-                        np.array(f["data_dm_time"], dtype=np.float32)
-                    )
-                    data_dt /= np.std(data_dt)
-                    data_dt -= np.median(data_dt)
-                    X[
-                        i,
-                    ] = np.reshape(data_ft, (*self.ft_dim, self.n_channels))
-                    Y[
-                        i,
-                    ] = np.reshape(data_dt, (*self.dt_dim, self.n_channels))
-            except KeyError:
-                print(ID)
+        for i, (data_ft, data_dt) in enumerate(list_IDs_temp):
+            # Process data_freq_time
+            data_ft = s.detrend(np.nan_to_num(data_ft.astype(np.float32).T))
+            data_ft /= np.std(data_ft)
+            data_ft -= np.median(data_ft)
+            # Process data_dm_time
+            data_dt = np.nan_to_num(data_dt.astype(np.float32))
+            data_dt /= np.std(data_dt)
+            data_dt -= np.median(data_dt)
+            X[i,] = np.reshape(data_ft, (*self.ft_dim, self.n_channels))
+            Y[i,] = np.reshape(data_dt, (*self.dt_dim, self.n_channels))
+
             y[i] = self.labels[indexes[i]]
+
         X[X != X] = 0.0
         Y[Y != Y] = 0.0
 
